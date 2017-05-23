566d7977e1ff99a226a5083977afd4ed3f778c51
Davi Alves <davi.alvesjunior@gmail.com>
2017-05-23 09:03:45 -0300

    Refatoração do mêtodo para tornar pedra clicávdl

--- js/Client/Models/Sprites/SpritePedra.js
+++ js/Client/Models/Sprites/SpritePedra.js
@@ -4,3 +4,16 @@ var SpritePedra = function(nome) {
     this.largura = 68;
     this.phaserSprite;
 }
+
+SpritePedra.prototype.CarregarSpritePhaser = function(posicao) {
+   this.phaserSprite = game.add.sprite(posicao.x, posicao.y, this.nome); 
+}
+
+SpritePedra.prototype.TornarSpriteClicavel = function(tornarPedraClicavel) {
+   this.phaserSprite.data = this;
+   tonarPedraClicavel.Tornar(this.phaserSprite);
+}
+
+SpritePedra.prototype.RenoverEventoClick = function() {
+   // TODO: Remover evento click do spritePhaser
+}
