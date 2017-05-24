27324c2c3791b269bf6e3fa120a8db41ad1ed8f0
Davi Alves <davi.alvesjunior@gmail.com>
2017-05-24 08:42:39 -0300

    Merge

--- js/Client/Models/Turno.js
+++ js/Client/Models/Turno.js
@@ -2,14 +2,24 @@ var Turno = function() {
 
 }
 
-Turno.prototype.AnalisarPedrasJogador() {
-
+Turno.prototype.AnalisarPedrasJogador(jogo) {
+    var possuiPedra = false;
+    jogo.jogador.ParaCadaPedra(function(pedra) {
+        var movimentos = jogo.tela.mesa.VerificarMovimentosPossiveisParaPedra(pedra);
+	pedra.TornarSpriteClicavel(jogo, pedra, movimentos);
+	possuiPedra = movimentos.length > 0;
+    });
+    jogo.tela.spriteComprar.TornarSpriteClicavel(jogo, possuiPedra);
 }
 
-Turno.prototype.Iniciar() {
-	
+Turno.prototype.Iniciar(jogo) {
+    // TODO: Iniciar turno;
 }
 
-Turno.prototype.Finalizar() {
-	
+Turno.prototype.Finalizar(jogo) {
+    jogo.tela.spriteComprar.RemoverEventoClick();
+    jogo.jogador.ParaCadaPedra(function(pedra) {
+        pedra.RenoverEventoClick();
+    }
+    // TODO: Passar a vez;
 }
