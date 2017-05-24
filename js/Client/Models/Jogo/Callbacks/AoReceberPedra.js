c461723f5ef9a5d348b21892857eef6f3676fa8b
Davi Alves <davi.alvesjunior@gmail.com>
2017-05-24 09:03:44 -0300

    Ajustes

--- js/Client/Models/Jogo/Callbacks/AoReceberPedra.js
+++ js/Client/Models/Jogo/Callbacks/AoReceberPedra.js
@@ -1,5 +1,9 @@
 var AoReceberPedra = function() {}
 AoReceberPedra.prototype.Disparar = function(jogo, data) {
     console.log("[JOGO] A pedra " + data.domino.value1 + "|" + data.domino.value2 + " foi recebida.");
-    jogo.AdicionarPedraComprada(data.domino);
+    var turno = new Turno();
+    turno.Finalizar(jogo);
+    jogo.AdicionarPedra(data.domino);
+    turno.AnalisarPedrasJogador(jogo);
+    turno.Iniciar();
 }
