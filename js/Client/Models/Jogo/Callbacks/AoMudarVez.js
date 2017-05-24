8a4905485ca3a20ca54a36228f716b950667c3f2
Davi Alves <davi.alvesjunior@gmail.com>
2017-05-24 08:59:41 -0300

    Mudanças para comportamento em turnos

--- js/Client/Models/Jogo/Callbacks/AoMudarVez.js
+++ js/Client/Models/Jogo/Callbacks/AoMudarVez.js
@@ -1,5 +1,9 @@
 var AoMudarVez = function() {}
 AoMudarVez.prototype.Disparar = function(jogo, data) {
     jogo.AlterarTurno(data.turn);
+    if (!jogo.PodeJogar()) return;
     jogo.notificacao.NotificarEstado(jogo);
+    var turno = new Turno();
+    turno.AnalisarPedrasJogador(jogo);
+    turno.Iniciar(jogo);
 }
