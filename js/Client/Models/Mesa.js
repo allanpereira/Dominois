64c190189e2fb7c324e2d25cc35f0988b960715e
Davi Alves <davi.alvesjunior@gmail.com>
2017-06-11 17:49:56 -0300

    Algunsoutros ajustes na pedra

--- js/Client/Models/Mesa.js
+++ js/Client/Models/Mesa.js
@@ -5,9 +5,15 @@
 var Mesa = function() {
     this.sprite = new SpriteMesa();
     
-    this.pedrasJogadas = [];
-    this.valorEsquerda;
-    this.valorDireita;
+    this.valorPonta = {
+		esquerda: null,
+		direita: null
+	}
+
+    this.pedras = {
+        esquerda: [],
+        direita: [],
+    }
 }
 
 Mesa.prototype.VerificarMovimentosPossiveisParaPedra = function(pedra) {
@@ -42,19 +48,19 @@ Mesa.prototype.JogarPedra = function(pedra, moveType) {
 	
 	switch (moveType) {
 		case MoveType.FirstDomino:
-			jogadaMesa;
+			jogadaMesa = new PrimeiraJogadaMesa().Jogar(pedra);
 			this.valorPonta.esquerda = pedra.valorSuperior;
 			this.valorPonta.direita = pedra.valorInferior;
 			this.pedras.esquerda(pedra);
 			this.pedras.direita(pedra);
 			break;
 		case MoveType.LeftSide:
-			jogadaMesa = new TentarJogarNaEsquerdaMesa().Jogar();
+			jogadaMesa = new TentarJogarNaEsquerdaMesa().Jogar(pedra, this.valorPonta.esquerda);
 			this.valorPonta.esquerda = jogadaMesa.valorPonta;
 			this.pedras.esquerda(pedra);
 			break;
 		case MoveType.RightSide:
-			jogadaMesa = new TentarJogarNaDeireitaMesa().Jogar();
+			jogadaMesa = new TentarJogarNaDeireitaMesa().Jogar(pedra, this.valorPonta.direita);
 			this.valorPonta.direita = jogadaMesa.valorPonta;
 			this.pedras.direita(pedra);
 			break;
