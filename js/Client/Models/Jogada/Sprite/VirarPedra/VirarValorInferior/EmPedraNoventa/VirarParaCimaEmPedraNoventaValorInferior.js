dc921c88f51c79c2f506ba43e6dfa21e3c036236
Davi Alves <davi.alvesjunior@gmail.com>
2017-06-12 01:25:19 -0300

    Inclusão de mais alguns arquivos para a lógica das jogadas

---
+++ js/Client/Models/Jogada/Sprite/VirarPedra/VirarValorInferior/EmPedraNoventa/VirarParaCimaEmPedraNoventaValorInferior.js
@@ -0,0 +1,11 @@
+var VirarParaBaixoEmPedraNoventaValorInferior = function() {}
+
+VirarParaBaixoEmPedraNoventaValorInferior.prototype.Jogar = function(pedra) {
+	console.log(this);
+	return new JogadaSprite
+	(
+		pedra.sprite.phaserSprite.position.x - pedra.sprite.largura,
+		pedra.sprite.phaserSprite.position.y + (pedra.sprite.altura + pedra.sprite.largura),
+		RotacaoSprite.CentoEOitenta
+	);
+}
