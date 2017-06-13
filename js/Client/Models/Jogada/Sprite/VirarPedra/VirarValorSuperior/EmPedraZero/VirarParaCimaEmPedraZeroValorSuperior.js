327ddec749da0d7b216a80b4258351861e16b518
Davi Alves <davi.alvesjunior@gmail.com>
2017-06-13 02:56:58 -0300

    Ajuste na virada de pedra para cima em Pedra Zero

--- js/Client/Models/Jogada/Sprite/VirarPedra/VirarValorSuperior/EmPedraZero/VirarParaCimaEmPedraZeroValorSuperior.js
+++ js/Client/Models/Jogada/Sprite/VirarPedra/VirarValorSuperior/EmPedraZero/VirarParaCimaEmPedraZeroValorSuperior.js
@@ -6,6 +6,6 @@ VirarParaCimaEmPedraZeroValorSuperior.prototype.Jogar = function(pedra) {
 	(
 		pedra.sprite.phaserSprite.position.x,
 		pedra.sprite.phaserSprite.position.y,
-		RotacaoSprite.NaoRotacionar
+		RotacaoSprite.CentoEOitenta
 	);
 }
