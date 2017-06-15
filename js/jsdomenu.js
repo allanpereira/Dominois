d466a2323aa608506944c58606d7794fb9a8ba54
Allan Pereira <allan.pereira.cunha@gmail.com>
2017-06-15 15:20:56 -0300

    Cache de objetos do menu

--- js/jsdomenu.js
+++ js/jsdomenu.js
@@ -1,37 +1,42 @@
 $(document).ready(function() {
+    var conteudo = $(".conteudo");
+    var list = $(".menu li");
+    var firstMenu = $(".menu li:first");
+    var firstTab = $(".conteudo:first");
 
-	//Quando carregar a pagina
-	$(".conteudo").hide(); //esta primeira linha servir para esconder as divs do contedo
-	$(".menu li:first").addClass("active").show(); //Ativar primeira aba
-	$(".conteudo:first").show('slow'); //abre a pagina suavemente
+    firstMenu.addClass("active").show(); // Ativa a primeira aba
+    
+    conteudo.hide();
+    firstTab.show('slow'); // Abre a página suavemente
 
-	//On Click Event
-	$(".menu li").click(function() {// quando clicar no link do menu ir executar a seguinte funo:
-		$(".menu li").removeClass("active"); //remove a class no link clicado para que no fica marcado
-		$(this).addClass("active"); //adiciona a classe no link clicado para que o usuario saiba em qual link ele esta
+    list.click(function() {
+        var self = $(this);
 
-		$(".conteudo").hide(); //esconder as divs do contedo
-		var activeaba = $(this).find("a").attr("href"); //Encontrar o valor do atributo rel para identificar a aba que esta ativa + contedo
-		$(activeaba).fadeIn('slow'); //efeito de fade in com o contedo ativo
-		return false;// o returno falso evita que ao clicar no link a pgina role para o topo.
-	});
+        list.removeClass("active"); // Remove a class no link clicado para que no fica marcado
+        self.addClass("active");    // Adiciona a classe no link clicado para que o usuario saiba em qual link ele esta
 
+        conteudo.hide(); // Esconde as divs de conteúdo
+        var tab = self.find("a").attr("href"); // Encontra o valor do atributo rel para identificar a aba que esta ativa + conteúdo
+        $(tab).fadeIn('slow');
+        
+        return false; // Evita que ao clicar no link a página role para o topo.
+    });
 });
 
 function atualizalista() {
-  var p = document.getElementById('lista');
-  var filhos = p.childNodes;
-  for( i = filhos.length - 1; i >= 0; i-- ) {
-    if( filhos[i].tagName == 'LI' ) {
-      p.removeChild( filhos[i] );
+    var p = document.getElementById('lista');
+    var filhos = p.childNodes;
+    for(var i = filhos.length - 1; i >= 0; i-- ) {
+        if( filhos[i].tagName == 'LI' ) {
+            p.removeChild( filhos[i] );
+        }
     }
-  }
 
-  var li = document.createElement('li');
-  li.innerHTML = 'seis';
-  p.appendChild(li);
+    var li = document.createElement('li');
+    li.innerHTML = 'seis';
+    p.appendChild(li);
 
-  var li = document.createElement('li');
-  li.innerHTML = 'sete';
-  p.appendChild(li);
+    var li = document.createElement('li');
+    li.innerHTML = 'sete';
+    p.appendChild(li);
 }
