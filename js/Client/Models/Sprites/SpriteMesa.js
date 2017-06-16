e418e51f9844441b76634f32b9b67a6d9c566820
Davi Alves <davi.alvesjunior@gmail.com>
2017-06-15 21:24:33 -0300

    Lógica para jogar movimentar a mesa conforme jogo

--- js/Client/Models/Sprites/SpriteMesa.js
+++ js/Client/Models/Sprites/SpriteMesa.js
@@ -2,15 +2,48 @@ var SpriteMesa = function() {
     var self = this;
     
     this.nome = "mesa";
-    
-    this.posicao = {
+        
+	this.auxiliarPedra = {
+		superior: 0,
+		inferior: 0,
+		direita: 0,
+		esquerda: 0,
+		centro: {
+			x: 0,
+			y: 0
+		},
+		Calcular: function() {
+			var esquerda = self.posicao.x + self.tamanho.largura*0.1;
+			var superior = self.posicao.x + self.tamanho.altura*0.1;
+
+			this.superior = esquerda;
+			this.esquerda = superior;
+			this.inferior = self.superior + self.tamanhoBase.altura;
+			this.direita = self.esquerda + self.tamanhoBase.largura;
+
+			this.centro.x = self.tamanho.largura/2.3;
+			this.centro.y = self.tamanho.altura/2.3;
+		}
+	}
+
+	this.posicaoBase = {
+        x: 100,
+        y: 300
+    };
+
+    this.tamanhoBase = {
+        altura: 900,
+        largura: 900
+    };
+
+	this.posicao = {
         x: 0,
         y: 0
     };
-    
-    this.tamanho = {
-        altura: 900,
-        largura: 600
+
+	this.tamanho = {
+        altura: 0,
+        largura: 0
     };
 
 	this.phaserSprite;
@@ -28,7 +61,7 @@ SpriteMesa.prototype.PrepararJogada = function(moveType, ladoPedra, pedraAnterio
 		case MoveType.FirstDomino:
 			this.proximaJogadaEsquerda = { jogada: new JogarParaEsquerdaOuParaBaixo() };
 			this.proximaJogadaDireita = { jogada: new JogarParaDireitaOuParaCima()};
-			return new PrimeiraJogada().Jogar(ladoPedra);
+			return new PrimeiraJogada().Jogar(this.auxiliarPedra.centro, ladoPedra);
 		
 		case MoveType.LeftSide:
 			return this.proximaJogadaEsquerda.jogada.Jogar(this.proximaJogadaEsquerda, ladoPedra, pedraAnterior, mesa);
@@ -39,13 +72,41 @@ SpriteMesa.prototype.PrepararJogada = function(moveType, ladoPedra, pedraAnterio
 }
 
 SpriteMesa.prototype.MoverPedra = function(jogadaSprite, pedra, pedraAnterior) {		
+	
 	pedra.sprite.phaserSprite.position.x = jogadaSprite.x;
 	pedra.sprite.phaserSprite.position.y = jogadaSprite.y;
 	pedra.sprite.phaserSprite.angle = jogadaSprite.rotacaoSprite;
 	pedra.sprite.rotacaoSprite = jogadaSprite.rotacaoSprite;
+
+	this.phaserSprite.addChild(pedra.sprite.phaserSprite);
 	console.log(pedra);
 }
 
 SpriteMesa.prototype.CarregarSpritePhaser = function(larguraTela, alturaTela) {
-	this.phaserSprite = game.add.tileSprite(0, 0, larguraTela, alturaTela, this.nome);
+
+	this.tamanho.largura = this.tamanhoBase.largura + 2*this.posicao.x;
+	this.tamanho.altura = this.tamanhoBase.altura + 2*this.posicao.y;
+	
+	if (larguraTela > this.tamanho.largura) this.tamanho.largura = larguraTela;
+	if (alturaTela > this.tamanho.altura) this.tamanho.altura = alturaTela;
+	
+
+	this.tamanho.largura *= 4;
+	this.tamanho.altura *= 4;
+	this.posicao.x = -0.3*this.tamanho.largura;
+	this.posicao.y = -0.3*this.tamanho.altura;
+
+	this.auxiliarPedra.Calcular();
+
+	this.phaserSprite = game.add.tileSprite
+	(
+		this.posicao.x,
+		this.posicao.y,
+		this.tamanho.largura,
+		this.tamanho.altura,
+		this.nome
+	);
+
+	this.phaserSprite.inputEnabled = true;
+	this.phaserSprite.input.enableDrag(false);
 }
